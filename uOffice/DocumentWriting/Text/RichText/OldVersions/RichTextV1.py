3891514540e138f64ad90e0f8940f02697900a9e
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-23 17:12:46 -0700

    RichTextV1.py
    An archive of the first version of the Rich Text definitions python script.

---
+++ uOffice/DocumentWriting/Text/RichText/OldVersions/RichTextV1.py
@@ -0,0 +1,43 @@
+# Start of script
+# List of Rich Text features
+""" Divider """
+# Normal - Normal text
+def normalText():
+  print("Normal text mode")
+# Bold - Bolded text
+def boldText():
+  print("Bold text mode")
+# Italics - Italic text
+def italicText():
+  print("Italic text mode")
+# Underline - Underlined text
+def underlineText():
+  print("Underlined text mode")
+# Overline - Overlined text, text with a line over it instead of under it
+def overlineText():
+  print("Overlined text mode")
+# Shadow - Adds a shadow to the text
+def shadowText():
+  print("Shadow text mode")
+# Code - Adds a code box to the text
+def codeText():
+  print("Code text mode")
+# Link - Makes the text a hyperlink
+def hyperlinkText():
+  print("Hyperlink mode")
+# Crossthrough - Crosses through the text with a line
+def crossthroughText():
+  print("Crossthrough text mode")
+# Strikethrough - Makes an X through a set of characters, there has to be at least 2 characters to be able to use the strikethrough effect
+def strikethroughText():
+  print("Strikethrough text mode")
+"""
+File version: 1 (Tuesday, June 23rd 2020 at 5:12 pm)
+Line count: 44
+File type: Python (*.py)
+"""
+# If someone were to run this file
+print("This file is not meant to be opened")
+noMoreExit = input("Press [ENTER] key to close")
+print("If the program still hasn't closed, click the close button, or perform a --taskkill")
+# End of script
