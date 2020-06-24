c2c354bda0051e8da9d8281a9e08a071717d64cc
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-23 17:37:56 -0700

    BackgroundV1.py
    An archive of the first version of the backgrounds file.

---
+++ uOffice/DocumentWriting/Layout/Background/OldVersions/BackgroundV1.py
@@ -0,0 +1,24 @@
+# Start of script
+
+"""
+
+With uOffice, you can change the background of the document (and the document itself separately) to anything you want.
+You can change it to a color, you can change it to a gradient or pattern, you can even change it to an image or a video.
+Backgrounds from Firefox are allowed as well. You can also add documents in the background, including script files, text files, binary, and other.
+
+"""
+
+# For people who run this file
+noMoreInput = input("You aren't supposed to open this file. Press [ENTER] to close it")
+print("The file should be closed now. If it is still not closing, press the close button, or use a -taskkill command")
+
+"""
+File info:
+
+File type: Python (*.py)
+Line count: 25
+File version: 1 (Tuesday, June 23rd 2020 at 5:37 pm)
+
+"""
+
+# End of script
